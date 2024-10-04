b01edbf708a71ea019366b1812da9e69e8c6b1d0
Radier Falcao <radierfalcao4@gmail.com>
2024-10-04 13:36:35 -0400

    Import dados

---
+++ pandas.py
@@ -0,0 +1,9 @@
+import pandas as pd
+#Funcionamento
+#Forma mais básica (muitas vezes não usaremos a forma mais básica)
+#dataframe = pd.read_csv(arquivo existente)
+
+
+
+vendas_df = pd.read_csv('dados/Contoso - Cadastro Produtos.csv')
+print(vendas_df)
